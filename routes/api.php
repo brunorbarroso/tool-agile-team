43c401280f6608f3c4a014c2343484bc23403579
Bruno <brunobinfo@gmail.com>
2018-08-13 20:19:59 -0300

    Add route to api function user register

--- routes/api.php
+++ routes/api.php
@@ -13,9 +13,10 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::group(['middleware' => 'api', 'prefix' => 'auth'], function ($router) {
+Route::group(['prefix' => 'auth', 'middleware'=>['cors', 'api']], function($router){
     Route::post('login', 'Auth\\AuthJWTController@login');
     Route::post('logout', 'Auth\\AuthJWTController@logout');
     Route::post('refresh', 'Auth\\AuthJWTController@refresh');
     Route::post('me', 'Auth\\AuthJWTController@me');    
+    Route::post('register', 'Auth\\AuthJWTController@register');
 });
