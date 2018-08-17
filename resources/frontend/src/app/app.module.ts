c855df38e4dc791e417a5fe4fc30d256ea37fdeb
Bruno <brunobinfo@gmail.com>
2018-08-17 19:17:49 -0300

    Add modules of profile and auth guard

--- resources/frontend/src/app/app.module.ts
+++ resources/frontend/src/app/app.module.ts
@@ -1,3 +1,4 @@
+// libraries
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
@@ -5,16 +6,20 @@ import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 import { ToastrModule } from 'ngx-toastr';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { environment } from './../environments/environment';
 
+// components
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { UserComponent } from './user/user.component';
 import { SignInComponent } from './user/sign-in/sign-in.component';
 import { SignUpComponent } from './user/sign-up/sign-up.component';
+import { ProfileComponent } from './user/profile/profile.component';
+
+// resources
 import { appRoutes } from './routes';
-
 import { UserService } from './user/shared/user.service';
-
+import { AuthGuard } from './auth/auth.guard';
 
 @NgModule({
   declarations: [
@@ -22,7 +27,8 @@ import { UserService } from './user/shared/user.service';
     HomeComponent,
     UserComponent,
     SignInComponent,
-    SignUpComponent
+    SignUpComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
@@ -30,9 +36,9 @@ import { UserService } from './user/shared/user.service';
     HttpClientModule,
     ToastrModule.forRoot(),
     BrowserAnimationsModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes, { enableTracing: !environment.production })
   ],
-  providers: [UserService],
+  providers: [UserService, AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
