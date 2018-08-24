04a9002c2c0aba7a36533d3d5040edcd786d5450
Bruno <brunobinfo@gmail.com>
2018-08-24 14:17:11 -0300

    Removed functions and added to base repository

--- app/Repositories/TaskRepository.php
+++ app/Repositories/TaskRepository.php
@@ -2,8 +2,9 @@
 
 use App\Task;
 use App\Contracts\TaskRepositoryInterface;
+use App\Repositories\BaseRepository;
 
-class TaskRepository implements TaskRepositoryInterface
+class TaskRepository extends BaseRepository implements TaskRepositoryInterface
 {
 
     protected $repository;
@@ -11,58 +12,6 @@ class TaskRepository implements TaskRepositoryInterface
     public function __construct(Task $repository)
     {
         $this->repository = $repository;
-    }
-
-    public function all()
-    {
-        return $this->repository->all();
-    }
-
-    public function create($attrs)
-    {
-        return $this->repository->create($attrs);
-    }
-
-    public function find($id)
-    {
-        return $this->repository->find($id);
-    }
-
-    public function findBy($attr, $column)
-    {
-        return $this->repository->where($attr, $column);
-    }
-
-    public function update($attrs)
-    {
-        return $this->fill($attrs);
-    }
-
-    public function delete($id)
-    {
-        return $this->repository->destroy($id);
-    }
-
-    public function paginate($limit)
-    {
-        return $this->repository->latest()->paginate($limit);
-    }
-
-    public function search($attrs, $limit = false)
-    {
-        try
-        {
-            if($limit && is_array($attrs))
-            {
-                return $this->repository->where($attrs[0], $attrs[1], $attrs[2])->paginate($limit);
-            }
-        
-            return $this->repository->where($attrs[0], $attrs[1], $attrs[2])->get();
-
-        } catch(Exception $ex)
-        {
-            throw new Exception('Not possible to search information.');
-        }
     }
 
 }
