8d68ea0a5ec089e4c6aa2959fe194ba93f465da0
Bruno <brunobinfo@gmail.com>
2018-08-31 20:11:11 -0300

    Create file of routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,12 @@
 <?php
 
 use Illuminate\Http\Request;
+use Dingo\Api\Routing\Router;
+
+use App\Http\Controllers\Api\ClassificationController;
+use App\Http\Controllers\Api\ParametersController;
+use App\Http\Controllers\Api\TasksController;
+use App\Http\Controllers\Auth\AuthJWTController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,10 +19,23 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::group(['prefix' => 'auth', 'middleware'=>['api']], function($router){
-    Route::post('login', 'Auth\\AuthJWTController@login');
-    Route::post('logout', 'Auth\\AuthJWTController@logout');
-    Route::post('refresh', 'Auth\\AuthJWTController@refresh');
-    Route::post('me', 'Auth\\AuthJWTController@me');    
-    Route::post('register', 'Auth\\AuthJWTController@register');
+$api = app(Router::class);
+
+if(!$api instanceof Router)
+    throw new RuntimeException("Api routing not configured");
+
+$api->version('v1', ['after'=>'cors'], function(Router $api) {
+
+    $api->group(['prefix' => 'auth'], function(Router $api) {
+        $api->post('login', AuthJWTController::class.'@login');
+        $api->post('register', AuthJWTController::class.'@register');
+        $api->post('logout', AuthJWTController::class.'@logout', ['middleware'=>'jwt.auth']);
+        $api->post('refresh', AuthJWTController::class.'@refresh', ['middleware'=>'jwt.auth']);
+        $api->post('me', AuthJWTController::class.'@me', ['middleware'=>'jwt.auth']);
+    });
+
+    $api->group(['middleware'=>'jwt.auth'], function(Router $api) {
+        $api->resource('tasks', TasksController::class, ['middleware'=>'can:create-task,view-task,update-task,delete-task']);
+    });
+    
 });
