327afee5fd0c4aaffd54b63efd6b5b3c7d13f493
Bruno <brunobinfo@gmail.com>
2018-08-31 20:12:16 -0300

    Added in Auth Service Provider the permissions

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -24,7 +24,47 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
+        
+        // Permission of CRUD Task
+        Gate::define('create-task', function ($user) {
+            return $user->hasAccess(['create-task']);
+        });
+        Gate::define('update-task', function ($user) {
+            return $user->hasAccess(['update-task']);
+        });
+        Gate::define('view-task', function ($user) {
+            return $user->hasAccess(['view-task']);
+        });
+        Gate::define('destroy-task', function ($user) {
+            return $user->hasAccess(['destroy-task']);
+        });
 
-        //
+        // Permission of CRUD User
+        Gate::define('create-user', function ($user) {
+            return $user->hasAccess(['create-user']);
+        });
+        Gate::define('update-user', function ($user) {
+            return $user->hasAccess(['update-user']);
+        });
+        Gate::define('view-user', function ($user) {
+            return $user->hasAccess(['view-user']);
+        });
+        Gate::define('destroy-user', function ($user) {
+            return $user->hasAccess(['destroy-user']);
+        });
+
+        // Permission of CRUD Parameter
+        Gate::define('create-parameter', function ($user) {
+            return $user->hasAccess(['create-parameter']);
+        });
+        Gate::define('update-parameter', function ($user) {
+            return $user->hasAccess(['update-parameter']);
+        });
+        Gate::define('view-parameter', function ($user) {
+            return $user->hasAccess(['view-parameter']);
+        });
+        Gate::define('destroy-parameter', function ($user) {
+            return $user->hasAccess(['destroy-parameter']);
+        });
     }
 }
